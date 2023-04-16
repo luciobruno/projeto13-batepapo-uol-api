a542bdfb6cdaccf588767d1363fa386cdcdc075d
Lúcio Bruno <luciobrunop20@hotmail.com>
2023-04-16 19:24:17 -0400

    POST /messages

--- src/app.js
+++ src/app.js
@@ -29,10 +29,10 @@ app.post("/participants", async (req, res) => {
     const validation = participantsSchema.validate(req.body, { abortEarly: false })
 
     if (validation.error) {
-        return res.status(422).send(validation.error)
+        return res.sendStatus(422)
     }
 
-    try {
+    try {   
         const participant = await db.collection("participants").findOne(req.body)
         if (participant) {
             return res.sendStatus(409)
@@ -59,6 +59,44 @@ app.get("/participants", async (req, res) => {
     } catch (err) {
         res.status(500).send(err.message)
     }
+})
+
+app.post("/messages", async (req, res) => {
+    const { to, text, type } = req.body
+    const from = req.headers.user
+
+    try {
+        const testFrom = await db.collection("participants").findOne({ name: from })
+        if (!testFrom) {
+            return res.sendStatus(422)
+        }
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+
+    const message = { from: from ,to: to, text: text, type: type, time: dayjs().format('HH:mm:ss') }
+
+    const messageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid('message', 'private_message'),
+        from: joi.string().required(),
+        time: joi.string().required()
+    })
+
+    const validation = messageSchema.validate(message, { abortEarly: false })
+
+    if (validation.error){
+        return res.sendStatus(422)
+    }
+
+    try{
+        await db.collection("messages").insertOne(message)
+        res.sendStatus(201)
+    }catch(err){
+        res.status(500).send(err.message)
+    }
+
 })
 
 app.listen(5000)
