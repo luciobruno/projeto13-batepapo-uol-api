aa5a7255709997d4a6fb1be2a7d404e87b3dbb26
Lúcio Bruno <luciobrunop20@hotmail.com>
2023-04-16 21:02:25 -0400

    fix:POST /messages

--- src/app.js
+++ src/app.js
@@ -79,7 +79,7 @@ app.post("/messages", async (req, res) => {
     const messageSchema = joi.object({
         to: joi.string().required(),
         text: joi.string().required(),
-        type: joi.string().valid('message', 'private_message'),
+        type: joi.string().valid('message', 'private_message').required(),
         from: joi.string().required(),
         time: joi.string().required()
     })
